e6d500efee7a6b92963957f497a2031f976b7b83
Duncan Staats <staatsd@wlhs.wlwv.k12.or.us>
2024-10-22 16:21:01 +0000

    new

---
+++ Main.py
@@ -0,0 +1,25 @@
+'''
+Name: Duncan Staats
+Date: 10/22/24
+Assignment: Unit 2 and 3 Project
+'''
+
+month1 = input("Enter the name of the month: ")
+month = month1.title()
+date = int(input("Enter the day (1-31): "))
+
+while True:
+    if month == "March" and 20 <= date <31 or month == "April" and 1 <= date <= 30 or month == "May" and 1 <= date <= 31 or month == "June" and 1 <= date <= 20:
+        print(f"{month} {date} is in Spring.")
+        break
+    elif month == "June" and 21<= date <= 30 or month == "July" and 1 <= date <= 31 or month == "August" and 1 <= date <= 31 or month == "September" and 1 <= date <= 21:
+        print(f"{month} {date} is in Summer")
+        break
+    elif month == "September" and 22 <= date <= 30 or month == "October" and 1 <= date <= 31 or month == "November" and 1 <= date <= 30 or month == "December" and 1 <= date <= 20:
+        print(f"{month} {date} is in Fall")
+        break
+    elif month == "December" and 21 <= date <= 31 or month == "January" and 1 <= date <= 31 or month == "Febuary" and 1 <= date <= 28 or month == "March" and 1 <= date <= 19:
+        print(f"{month} {date} is in Winter")
+        break
+    print("You entered in a date that doesn't exist. Try agian")
+        
